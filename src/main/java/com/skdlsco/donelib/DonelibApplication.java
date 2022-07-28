54563021fb0d64e930bdae1df8716ad2c73c2558
ina <nicnic4102@gmail.com>
2022-07-28 22:11:14 +0900

    init

---
+++ src/main/java/com/skdlsco/donelib/DonelibApplication.java
@@ -0,0 +1,13 @@
+package com.skdlsco.donelib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonelibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonelibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skdlsco/donelib/DonelibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skdlsco.donelib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonelibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
