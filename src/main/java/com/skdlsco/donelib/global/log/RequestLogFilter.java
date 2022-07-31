d85e61e643c44b5ade9408db4e84e47672eadef3
ina <nicnic4102@gmail.com>
2022-08-01 02:24:08 +0900

    add request log filter

---
+++ src/main/java/com/skdlsco/donelib/global/log/RequestLogFilter.java
@@ -0,0 +1,28 @@
+package com.skdlsco.donelib.global.log;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class RequestLogFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            log.info("addr={} request-uri={}", request.getRemoteAddr(), ServletUriComponentsBuilder.fromCurrentRequestUri().build().toUriString());
+            filterChain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
