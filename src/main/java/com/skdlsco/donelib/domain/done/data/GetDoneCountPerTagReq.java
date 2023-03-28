5de6294ba7f586f2e2cef3b40e56cc09edc74531
skdlsco <nicnic4102@gmail.com>
2023-03-28 15:45:23 +0900

    [FIX] month field not null 추가

--- src/main/java/com/skdlsco/donelib/domain/done/data/GetDoneCountPerTagReq.java
+++ src/main/java/com/skdlsco/donelib/domain/done/data/GetDoneCountPerTagReq.java
@@ -4,11 +4,13 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
 @Builder
 public class GetDoneCountPerTagReq {
+    @NotNull
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate month;
 }
