5fdcbd8b093a4fc51949a99d0efdc096790d6388
skdlsco <nicnic4102@gmail.com>
2023-04-11 14:37:03 +0900

    [FIX] validate-deploy.sh 수정
    10초 간격으로 6번 체크하도록 수정

--- scripts/validate-deploy.sh
+++ scripts/validate-deploy.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/bash
 
-curl localhost:8080/actuator/health | grep UP
+try=0
+while :
+do
+  curl localhost:8080/actuator/health | grep UP
+  SUCCESS=$?
+  if [ $SUCCESS -eq 0 ]; then
+    break
+  fi
+  if [ $try -gt 4 ]; then
+    break
+  fi
+  ((try++))
+  sleep 10
+done
+exit $SUCCESS
